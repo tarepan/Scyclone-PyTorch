132273980810c824aeb9d5872aee0d3c2aeb06c4
Tarepan <tarepan5884@gmail.com>
2020-11-14 11:11:26 +0000

    Add CI

---
+++ .github/actions/BatchJobRegistration/index.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const core = require('@actions/core');
+const AWS = require('aws-sdk');
+
+async function run() {
+    try {
+        // Get inputs
+        const pathJobDefInput = core.getInput("path-task-definition", {
+            required: true,
+        });
+        const adressContainer = core.getInput("adress-container", { required: true });
+
+        // Parse the task definition
+        const jobDefPath = path.isAbsolute(pathJobDefInput)
+            ? pathJobDefInput
+            : path.join(process.env.GITHUB_WORKSPACE ?? "", pathJobDefInput);
+        if (!fs.existsSync(jobDefPath)) {
+            throw new Error(`Task definition file does not exist: ${pathJobDefInput}`);
+        }
+        const baseJobDef = JSON.parse(
+            fs.readFileSync(jobDefPath, "utf-8")
+        );
+
+        // Template Docker image adress
+        baseJobDef.containerProperties.image = adressContainer
+
+        // Register jobDef to Batch
+        const batch = new AWS.Batch();
+        await batch.registerJobDefinition(baseJobDef).promise();
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
